caa22070eba0899b027d5af8299684ce4e02edcd
Bae Seo Hyeon <bae4614@gmail.com>
2021-08-22 21:55:38 +0900

    feat: vue.config.js update

--- dist/vue.config.js
+++ dist/vue.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-    publicPath: '/todolist_new',
-}
+    outputDir: './docs',
+    publicPath: '/todolist/',
+    devServer: { 
+      ///api 요청이 있을 때 해당 요청을 backend 쪽의 /api로 돌려주는 proxy 설정
+          proxy: { 
+                  '/api': { 
+                            target: 'http://localhost:3000/api',
+                            changeOrigin: true, 
+                            pathRewrite: { 
+                                        '^/api': ''
+                                      } 
+                          } 
+                } 
+        },
+    //frontend 프로젝트를 build했을 때 output이 ../backend/public에 위치
+      outputDir: '../backend/public',
+  }
+  
